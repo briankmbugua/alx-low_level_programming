ec7fa2357a3e03bb4db2cd55a8f9bce5c13968fc
briankmbugua <briankmbuguak@gmail.com>
2022-09-10 15:09:39 +0300

    using puts in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,2 @@
 #include <stdio.h>
-int main() {
-	char mychar = "Programming is like building a multilingual puzzle";
-		puts(mychar);
-		return 0;
-}
+int main() { puts("Programming is like building a multilingual puzzle") return 0;}
