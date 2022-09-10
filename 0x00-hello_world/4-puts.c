9f04b8658a2bc267aec100aae0398a80a742e873
briankmbugua <briankmbuguak@gmail.com>
2022-09-10 15:48:04 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,7 @@
 #include <stdio.h>
-int main() { puts("\"Programming is like building a multilingual puzzle"); return 0;}
+/* entry point */
+int main(void)
+{
+/* output a string */
+puts("\"Programming is like building a multilingual puzzle");
+return (0); }
