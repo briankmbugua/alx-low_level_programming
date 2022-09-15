3103092dd216dcca0051b86107b00018856400bd
briankmbugua <briankmbuguak@gmail.com>
2022-09-15 03:59:39 +0300

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - find abs value of an int
+ * @x: int to be checked
+ * Return: absolute value of int
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+	{
+	return (x);
+	}
+	return (-x)
+}
